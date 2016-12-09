7c5a159fb378db811fc7c044c3603c99048ada9f
yewshong <yewshong@gmail.com>
2016-12-09 16:27:01 +0800

    1. add Tnak pawn, TankController C++ class (Lecture 116)

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlyerController not possesing a tank"));
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *ControlledTank->GetName());
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
